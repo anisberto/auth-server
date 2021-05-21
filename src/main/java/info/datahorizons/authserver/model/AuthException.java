61b5cf266ad767e585c4fc061f7915193f55340d
Anisberto Reis <anisberto.nascimento@goias.gov.br>
2021-05-21 11:08:07 -0300

    Config lombok

--- src/main/java/info/datahorizons/authserver/model/AuthException.java
+++ src/main/java/info/datahorizons/authserver/model/AuthException.java
@@ -1,5 +1,12 @@
 package info.datahorizons.authserver.model;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class AuthException extends RuntimeException{
 
 	private static final long serialVersionUID = -5067913166715145328L;
@@ -9,10 +16,6 @@ public class AuthException extends RuntimeException{
 	public static enum Type{
 		WRONG_PASS, INVALID_EMAIL, EMAIL_ALREADY_EXISTS, USER_NOT_FOUND, PASS_REQUIRED}
 	
-	public AuthException() {
-		super();
-	}
-
 	public AuthException(String message, Throwable cause) {
 		super(message, cause);
 	}
@@ -24,14 +27,6 @@ public class AuthException extends RuntimeException{
 
 	public AuthException(Throwable cause) {
 		super(cause);
-	}
-
-	public Type getType() {
-		return type;
-	}
-
-	public void setType(Type type) {
-		this.type = type;
 	}
 
 	
